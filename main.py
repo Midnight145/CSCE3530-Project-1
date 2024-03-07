8038a872060de67f3cd20f2e00c31eba50767a05
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-03-06 21:03:54 -0600

    update private keys to use sqlite db, move key ids to int instead of uuid4

--- main.py
+++ main.py
@@ -1,43 +1,36 @@
-import json
-import typing
-
 from fastapi import FastAPI
+from jwcrypto import jwk
 
 import util
 
 app = FastAPI()
 
-GeneratedRSAKey: typing.TypeAlias = dict[str, str]  # Type alias for the generated RSA key for readability
-GeneratedJWK: typing.TypeAlias = dict[str, str]  # Type alias for the generated JWK for readability
-
-keys: list[GeneratedRSAKey] = []  # List to store the JWK keys
-
 
 @app.post("/auth")
-async def auth(expired: bool = False) -> GeneratedJWK:
+async def auth(request: util.AuthRequest, expired: bool = False) -> dict[str, str]:
     """
     Authentication endpoint, generates a JWT token
+    :param request: The request object containing the username and password
     :param expired: Whether the key should be expired
     :return: The created JWT
     """
-    key, token = util.generate_jwt(pubkey, privkey, expired)  # generate the JWT token
-    if key:
-        keys.append(json.loads(key.export()))
+    key, token = util.generate_jwt_pair(request, expired)  # generate the JWT token
 
     return {"jwt": token.serialize()}
 
 
 @app.get("/.well-known/jwks.json")  # for now, we don't *need* to store the keys, we can generate them on the fly
-def get_jwks() -> dict[str, list[GeneratedRSAKey]]:
+def get_jwks() -> dict[str, list[jwk.JWK]]:
     """
     jwks endpoint
     :return: The list of valid keys
     """
-    return {"keys": keys}
+    return {"keys": util.get_jwks()}
 
 
-pubkey, privkey = util.init_keys()  # generate the keys on startup
+util.init()  # initialize the database and generate dummy keys
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="localhost", port=8080, reload=True)
--- util.py
+++ util.py
@@ -1,59 +1,42 @@
 import datetime
-import os
-from uuid import uuid4
+import sqlite3
 
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from jwcrypto import jwk, jwt
+from pydantic import BaseModel
 
 
-def init_keys() -> (rsa.RSAPublicKey, rsa.RSAPrivateKey):
+class AuthRequest(BaseModel):
     """
-    Initialize the RSA keys on startup, creating them if necessary
-    :return: Tuple of the public and private key objects
+    The authentication request object for /auth
+    format:
+    {
+        "username": "example_user",
+        "password": "example_password"
+    }
     """
-    if os.path.exists('private.pem') and os.path.exists('public.pem'):
-        with open('public.pem', 'rb') as f:
-            public = serialization.load_pem_public_key(f.read())
-        with open('private.pem', 'rb') as f:
-            private = serialization.load_pem_private_key(f.read(), password=None)
-        return public, private
-    else:
-        return create_keys()  # keys don't exist, create them
+    username: str
+    password: str
 
 
-def create_keys() -> (rsa.RSAPublicKey, rsa.RSAPrivateKey):
+def init() -> None:
     """
-    Create the RSA keys if they don't exist on disk
-    :return: Tuple of the public and private key objects
+    Initialize the database and generate the keys. Should only be called once at the start of the program
+    :return: None
     """
-    private = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
-    )
-    public = private.public_key()
-
-    with open('private.pem', 'wb+') as f:
-        f.write(
-            private.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption()
-            )
-        )
-    with open('public.pem', 'wb+') as f:
-        f.write(
-            public.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo
-            )
-        )
-    return public, private
+    db = sqlite3.connect('totally_not_my_privateKeys.db')
+    db.execute("CREATE TABLE IF NOT EXISTS keys (kid INTEGER PRIMARY KEY AUTOINCREMENT, "
+               "key TEXT, exp INTEGER NOT NULL DEFAULT 0)")
+    db.commit()
+    db.close()
+    generate_jwt_pair(AuthRequest(username="expired_user", password="expired_password"), True)
+    generate_jwt_pair(AuthRequest(username="userABC", password="password123"), False)
 
 
-def generate_rsa_key(pubkey: rsa.RSAPublicKey, kid: str) -> jwk.JWK:
+def generate_jwk(pubkey: rsa.RSAPublicKey, kid: str) -> jwk.JWK:
     """
-    Generate a new RSA key and add it to the list of valid keys
+    Generate a new JWK and add it to the list of valid keys
     :param pubkey: The RSA public key
     :param kid: Key ID to use for the new key
     :return: The created JWK
@@ -63,46 +46,82 @@ def generate_rsa_key(pubkey: rsa.RSAPublicKey, kid: str) -> jwk.JWK:
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     )
     jwk_key = jwk.JWK.from_pem(pem)
-    jwk_key.kid = kid  # assign the key ID to the JWK key
-    # keys.append(json.loads(jwk_key.export()))  # add the key to the list
+    jwk_key.kid = kid  # actually set the kid
     return jwk_key
 
 
-def generate_jwt(pubkey: rsa.RSAPublicKey, privkey: rsa.RSAPrivateKey, expired: bool) -> (jwk.JWK, jwt.JWT):
+def generate_jwt_pair(request: AuthRequest, expired: bool) -> tuple[jwk.JWK, jwt.JWT]:
     """
-    Generate a new JWT token
-    :param pubkey: The RSA public key
-    :param privkey: The RSA private key
+    Generate a new JWK and JWT pair
+    :param request: The request object containing the username and password
     :param expired: Whether the key should be expired
-    :return:
+    :return: The created JWK and JWT
     """
-    kid = str(uuid4())
-    if not expired:
-        jwk_key = generate_rsa_key(pubkey, kid)  # generate a new key, add to the list
-    else:
-        jwk_key = None  # no need to generate a new key if it's expired
+    privkey = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    pem = privkey.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
 
-    # offset used to set the expiration time +/- 30 minutes
-    offset = 30 * 60 if not expired else -30 * 60
+    if expired:  # generate expiry timestamp
+        expiry = datetime.datetime.now().timestamp() + -30*60
+    else:
+        expiry = datetime.datetime.now().timestamp() + 60*60
+
+    kid = save_key(pem, int(expiry))  # save the key to the database
+    jwk_key = generate_jwk(privkey.public_key(), kid)  # generate a new key, add to the list
 
     header = {
         "alg": "RS256",
         "kid": kid
     }
     claims = {
-        "sub": "example_user",
+        "sub": request.username,
         "kid": kid,
         # I can just add offset to change expiry instead of playing with timedeltas
-        "exp": datetime.datetime.now().timestamp() + offset
+        "exp": expiry
     }
 
     token = jwt.JWT(header=header, claims=claims)
 
-    pem = privkey.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
     jwk_privkey = jwk.JWK.from_pem(pem)
     token.make_signed_token(jwk_privkey)
     return jwk_key, token
+
+
+def save_key(key: bytes, expiry: int) -> str:
+    """
+    Saves the private key to the database
+    :param key: The private key to save
+    :param expiry: The expiry timestamp
+    :return: the key ID of the saved key (The primary key for that row)
+    """
+    db = sqlite3.connect('totally_not_my_privateKeys.db')
+    db.execute("INSERT INTO keys (key, exp) VALUES (?, ?)", (key, expiry))
+    db.commit()
+    # have to do this to get the kid since it's autoincrement
+    kid = db.execute("SELECT kid FROM keys WHERE key = ?", (key,)).fetchone()[0]
+
+    db.close()
+    return str(kid)  # the grader expects a string, so I typecast it here instead of everywhere else
+
+
+def get_jwks() -> list[jwk.JWK]:
+    """
+    :return: The list of valid keys
+    """
+    db = sqlite3.connect('totally_not_my_privateKeys.db')
+    keys = db.execute("SELECT kid, key FROM keys").fetchall()
+    db.close()
+
+    jwks = []
+    for i in keys:
+        # We have to regenerate the JWK from the private key
+        privkey = serialization.load_pem_private_key(i[1], password=None)
+        jwks.append(generate_jwk(privkey.public_key(), str(i[0])))
+
+    return jwks

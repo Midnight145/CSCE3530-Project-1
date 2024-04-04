4758c8309186284cde7bbc47839bbab05216e35a
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-04-04 17:31:19 -0500

    update unittests

--- tests.py
+++ tests.py
@@ -1,78 +1,95 @@
 import datetime
 import json
+import sqlite3
 import unittest
 
-from jwcrypto import jwk, jwt
-
-from util import init_keys, create_keys, generate_rsa_key, generate_jwt
+from util import generate_jwt_pair, AuthRequest
 
 
 class TestUtil(unittest.TestCase):
-    def test_init_keys(self):
-        public, private = init_keys()
-        print("verifying keys")
-        self.assertIsNotNone(public)
-        self.assertIsNotNone(private)
-        print("keys are not None")
+    def test_generate_jwk_pair(self):
+        jwk_key, token = generate_jwt_pair(AuthRequest(username="example_user", password="example_password"), False)
+        self.assertIsNotNone(jwk_key)
+        self.assertIsNotNone(token)
+        print("generated jwk_key and token successfully")
 
-    def test_create_keys(self):
-        public, private = create_keys()
-        print("creating keys")
-        self.assertIsNotNone(public)
-        self.assertIsNotNone(private)
-        print("keys are not None")
+    def test_key_table(self):
+        db = sqlite3.connect('totally_not_my_privateKeys.db')
+        db.row_factory = sqlite3.Row
+        resp = db.execute("PRAGMA TABLE_INFO(keys)").fetchall()
+        db.close()
+        self.assertTrue(len(resp), 3)
+        print("keys table has 3 columns")
+        correct = {
+            "kid": "INTEGER",
+            "key": "BLOB",
+            "exp": "INTEGER"
+        }
+        for i in resp:
+            self.assertIn(i['name'], correct)
+            self.assertEqual(i['type'], correct[i['name']])
+        print("keys table has correct columns")
 
-    def test_generate_rsa_key(self):
-        public, private = init_keys()
-        print("generating rsa key with kid test_kid")
-        jwk_key = generate_rsa_key(public, 'test_kid')
-        self.assertEqual(jwk_key.kid, 'test_kid')
-        print("key kid is test_kid")
+    def test_user_table(self):
+        db = sqlite3.connect('totally_not_my_privateKeys.db')
+        db.row_factory = sqlite3.Row
+        resp = db.execute("PRAGMA TABLE_INFO(users)").fetchall()
+        db.close()
+        self.assertTrue(len(resp), 6)
+        print("users table has 6 columns")
+        correct = {
+            "id": "INTEGER",
+            "username": "TEXT",
+            "password_hash": "TEXT",
+            "email": "TEXT",
+            "date_registered": "TIMESTAMP",
+            "last_login": "TIMESTAMP"
+        }
+        for i in resp:
+            self.assertIn(i['name'], correct)
+            self.assertEqual(i['type'], correct[i['name']])
+        print("users table has correct columns")
 
-    def test_generate_jwt(self):
-        public, private = init_keys()
-        print("generating jwt")
-        jwk_key, token = generate_jwt(public, private, False)
-        self.assertEqual(jwk_key.kid, json.loads(token.header)['kid'])
-        self.assertEqual(json.loads(token.claims)['sub'], 'example_user')
-        print("jwt generated and is valid")
-
-    def test_generate_rsa_key_different_inputs(self):
-        public, private = init_keys()
-        print("generating rsa key with kid different_kid")
-        jwk_key = generate_rsa_key(public, 'different_kid')
-        self.assertEqual(jwk_key.kid, 'different_kid')
-        print("key kid is different_kid")
+    def test_auth_logs_table(self):
+        db = sqlite3.connect('totally_not_my_privateKeys.db')
+        db.row_factory = sqlite3.Row
+        resp = db.execute("PRAGMA TABLE_INFO(auth_logs)").fetchall()
+        db.close()
+        self.assertTrue(len(resp), 3)
+        print("auth_logs table has 4 columns")
+        correct = {
+            "id": "INTEGER",
+            "request_ip": "TEXT",
+            "request_timestamp": "TIMESTAMP",
+            "user_id": "INTEGER"
+        }
+        for i in resp:
+            self.assertIn(i['name'], correct)
+            self.assertEqual(i['type'], correct[i['name']])
+        print("auth_logs table has correct columns")
 
     def test_expired_key_is_None(self):
-        public, private = init_keys()
         print("generating expired jwt")
-        jwk_key, token = generate_jwt(public, private, True)
+        jwk_key, token = generate_jwt_pair(AuthRequest(username="expired_user", password="expired_password"), True)
         self.assertIsNone(jwk_key)
         print("jwk_key is None")
 
     def test_expired_key_is_expired(self):
-        public, private = init_keys()
-        jwk_key: jwk.JWK
-        token: jwt.JWT
         print("ensuring expired jwt is expired")
-        jwk_key, token = generate_jwt(public, private, True)
+        jwk_key, token = generate_jwt_pair(AuthRequest(username="expired_user", password="expired_password"), True)
         self.assertTrue(json.loads(token.claims)["exp"] < datetime.datetime.now().timestamp())
         print("jwt is expired")
 
     def test_valid_key_is_not_expired(self):
-        public, private = init_keys()
-        jwk_key: jwk.JWK
-        token: jwt.JWT
         print("ensuring valid jwt is not expired")
-        jwk_key, token = generate_jwt(public, private, False)
+        jwk_key, token = generate_jwt_pair(AuthRequest(username="example_user", password="example_password"), False)
         self.assertTrue(json.loads(token.claims)["exp"] > datetime.datetime.now().timestamp())
         print("jwt is not expired")
 
     def test_valid_key_is_not_None(self):
-        public, private = init_keys()
         print("ensuring valid jwt is not None")
-        jwk_key, token = generate_jwt(public, private, False)
+        jwk_key, token = generate_jwt_pair(AuthRequest(username="example_user", password="example_password"), False)
+        self.assertTrue(json.loads(token.claims)["exp"] > datetime.datetime.now().timestamp())
         self.assertIsNotNone(jwk_key)
         print("jwk_key is not None")
 
